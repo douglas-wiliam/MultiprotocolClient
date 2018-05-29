f2d34e4b17373a479fab583b0a9282832537815a
Douglas Wiliam <dougl.wil@gmail.com>
2018-05-29 17:45:49 -0300

    Cliente SMTP alterado para permitir autenticacao com usuario e senha

--- src/Base/SMTPClient.java
+++ src/Base/SMTPClient.java
@@ -1,38 +1,107 @@
 package Base;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
+/**
+ *
+ * @author douglas, diego
+ */
 public class SMTPClient {
 
-    private final String msg;
-    private final String remet;
-    private final String dest;
-    private final String server;
+    Socket socket;
+    private static DataOutputStream dos;
+    private BufferedReader br;
     private final int porta;
-    
-    private SMTP mail;
+    private final int delay;
+    private final String server;
+    private final String usuario;
+    private final String senha;
+    private final String authUsuario;
+    private final String authSenha;
+    private final String destinatario;
 
-    private BufferedReader in;
-    private PrintWriter out;
+    private final InetAddress serverHost;
+    private final InetAddress localHost;
 
-    public SMTPClient(String msg, String remet, String dest, String server, int porta) throws UnknownHostException, IOException {
-        this.msg = msg;
-        this.remet = remet;
-        this.dest = dest;
+    public SMTPClient(String server, int porta, String usuario, String senha, String destinatario) throws UnknownHostException {
         this.server = server;
         this.porta = porta;
+        delay = 1000;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.destinatario = destinatario;
 
-        mail = new SMTP(server, porta);
+        localHost = InetAddress.getLocalHost();
+        serverHost = InetAddress.getByName(server);
 
-        if (mail != null) {
-            if (mail.send(this.msg, this.remet, this.dest)) {
-                System.out.println("Conexao com o servidor SMTP finalizada.");
-            } else {
-                System.out.println("Conexao com o servidor SMTP falhou!");
-            }
-        }
+        authUsuario = Base64.getEncoder().encodeToString(usuario.getBytes(StandardCharsets.UTF_8));
+        authSenha = Base64.getEncoder().encodeToString(senha.getBytes(StandardCharsets.UTF_8));
+
     }
+
+    public void conecta() throws IOException, InterruptedException {
+
+        socket = new Socket(serverHost, porta);
+
+        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        (new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    String linha;
+                    while ((linha = br.readLine()) != null) {
+                        System.out.println("SERVER: " + linha);
+                    }
+                } catch (IOException ex) {
+
+                    ex.printStackTrace();
+                }
+
+            }
+        })).start();
+
+        dos = new DataOutputStream(socket.getOutputStream());
+
+        envia("EHLO " + localHost + "\r\n");
+        Thread.sleep(delay);
+        envia("AUTH LOGIN\r\n");
+        Thread.sleep(delay);
+        envia(authUsuario + "\r\n");
+        Thread.sleep(delay);
+        envia(authSenha + "\r\n");
+        Thread.sleep(delay);
+        envia("MAIL FROM:<" + usuario + ">\r\n");
+        Thread.sleep(delay);
+        envia("RCPT TO:<" + destinatario + ">\r\n");
+        Thread.sleep(delay);
+        envia("DATA\r\n");
+        Thread.sleep(delay);
+        envia("From: " + usuario + "\r\n");
+        Thread.sleep(delay);
+        envia("To: " + destinatario + "\r\n");
+        Thread.sleep(delay);
+        envia("Subject: Teste de Envio de Email -  Cliente SMTP\r\n");
+        Thread.sleep(delay);
+        envia("Diego Raian e Douglas Wiliam\r\n");
+        Thread.sleep(delay);
+        envia(".\r\n");
+        Thread.sleep(delay);
+        envia("QUIT\r\n");
+    }
+
+    private static void envia(String comando) throws IOException {
+        dos.writeBytes(comando);
+        System.out.println("CLIENTE: " + comando);
+    }
+
 }
